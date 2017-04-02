2c51f60b65a6964ee8295c41a118dd1197d9cf61
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-02 23:09:05 +0200

    Tweak documentation

--- R/userinfo.R
+++ R/userinfo.R
@@ -1,15 +1,18 @@
-#' Lookup UID/GRP info
+#' User / Group Info
 #' 
-#' Lookup user or group info via UID/GID or user/group name
+#' Lookup a user or group info via user uid/name or group gid/name.
 #' 
 #' @export
 #' @rdname userinfo
+#' @name userinfo
 #' @param uid user ID (integer) or name (string)
 #' @useDynLib unix R_user_info
-#' @examples # Get info about root
-#' user_info(0)
-#' group_info(0)
-user_info <- function(uid = me()){
+#' @references [GETPWNAM(3)](http://man7.org/linux/man-pages/man3/getpwnam.3.html)
+#' [GETGRNAM(3)](http://man7.org/linux/man-pages/man3/getgrnam.3.html)
+#' @examples # Get info current user
+#' user_info()
+#' group_info()
+user_info <- function(uid = getuid()){
   if(is.numeric(uid))
     uid <- as.integer(uid)
   stopifnot(length(uid) > 0, is.numeric(uid) || is.character(uid))
@@ -21,16 +24,10 @@ user_info <- function(uid = me()){
 #' @rdname userinfo
 #' @param gid group ID (integer) or name (string)
 #' @useDynLib unix R_group_info
-group_info <- function(gid = me()){
+group_info <- function(gid = getgid()){
   if(is.numeric(gid))
     gid <- as.integer(gid)  
   stopifnot(length(gid) > 0, is.integer(gid) || is.character(gid))
   out <- .Call(R_group_info, gid)
   structure(out, names = c("name", "passwd", "gid", "members"))
-}
-
-#' @export
-#' @rdname userinfo
-me <- function(){
-  Sys.info()[["effective_user"]]
 }
