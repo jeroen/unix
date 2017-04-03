42a48fdc25b30853d0c83e9d64566de7c2033f6c
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-03 20:44:08 +0200

    Port affinity stuff

---
+++ R/affinity.R
@@ -0,0 +1,72 @@
+#' Process Affinity
+#'
+#' Get/set the process's CPU affinity mask. The affinity mask binds the process to
+#' specific core(s) within the machine. Not supported on all systems, [has_affinity()]
+#' shows if this is available.
+#'
+#' Setting a process afinity allows for restricting the process to only use certain
+#' cores in the machine. The cores are indexed by the operating system as 1 to [ncores()].
+#' Calling [setaffinity()] with no arguments resets the process to use any of the 
+#' available cores.
+#'
+#' Note that setaffinity is different from setting r_limit values in the sense that
+#' it is not a one-way process. An unprivileged user can change the process affinity
+#' to any value. In order to 'lock' an affinity value, one would have to manipulate
+#' Linux capability value for CAP_SYS_NICE.
+#'
+#' @name affinity
+#' @rdname affinity
+#' @param cpus Which cpu cores to bind to: vector of integers between 1 and [ncores()]
+#' @references [SCHED_SETAFFINITY(2)](http://man7.org/linux/man-pages/man2/sched_setaffinity.2.html)
+#' @examples \dontrun{
+#' # Current affinity
+#' ncores()
+#' getaffinity();
+#' getaffinity_count();
+#' 
+#' # Restrict process to core number 1.
+#' setaffinity(1)
+#' getaffinity()
+#' 
+#' # Reset
+#' setaffinity()
+#' getaffinity()
+#' }
+#' @useDynLib unix R_setaffinity
+#' @rdname affinity
+#' @export
+setaffinity <- function(cpus = 1:ncores()){
+  cpus <- as.integer(cpus)
+  if(any(cpus > ncores()))
+    stop("CPU number out of range. Available cpus: ", paste(1:ncores(), collapse=","));
+  .Call(R_setaffinity, cpus);
+  getaffinity()
+}
+
+#' @useDynLib unix R_getaffinity_count
+#' @rdname affinity
+#' @export
+getaffinity_count <- function(){
+  .Call(R_getaffinity_count)
+}
+
+#' @useDynLib unix R_getaffinity
+#' @rdname affinity
+#' @export
+getaffinity <- function(){
+  which(.Call(R_getaffinity))
+}
+
+#' @useDynLib unix R_has_affinity
+#' @rdname affinity
+#' @export
+has_affinity <- function(){
+  .Call(R_has_affinity)
+}
+
+#' @useDynLib unix R_ncores
+#' @rdname affinity
+#' @export
+ncores <- function(){
+  .Call(R_ncores)
+}
---
+++ src/affinity.c
@@ -0,0 +1,72 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <unistd.h>
+#include <Rinternals.h>
+
+// TODO: better feature test?
+#ifdef cpu_set_t
+#define HAS_AFFINITY
+#endif
+
+void bail_if(int err, const char * what);
+
+SEXP R_has_affinity(){
+#ifdef HAS_AFFINITY
+  return ScalarLogical(TRUE);
+#else
+  return ScalarLogical(FALSE);
+#endif
+}
+
+SEXP R_getaffinity_count() {
+#ifdef HAS_AFFINITY
+  //init the cpu mask
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  bail_if(sched_getaffinity(0, sizeof mask, &mask) < 0, "sched_getaffinity()");
+
+  //count number of enabled cores
+  return ScalarInteger(CPU_COUNT(&mask));
+#else 
+  Rf_error("affinity not supported on this system");
+#endif //HAS_AFFINITY
+}
+
+SEXP R_getaffinity() {
+#ifdef HAS_AFFINITY
+  //init the cpu mask
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  bail_if(sched_getaffinity(0, sizeof mask, &mask) < 0, "sched_getaffinity()");
+
+  //read values
+  int len = sysconf(_SC_NPROCESSORS_ONLN);
+  SEXP out = PROTECT(allocVector(LGLSXP, len));
+  for(int i = 0; i < len; i++)
+    LOGICAL(out)[i] = CPU_ISSET(i, &mask);
+  UNPROTECT(1);
+  return(out);
+#else 
+  Rf_error("affinity not supported on this system");
+#endif //HAS_AFFINITY  
+}
+
+SEXP R_setaffinity(SEXP cpus) {
+#ifdef HAS_AFFINITY
+  //init the cpu mask
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+
+  //NOTE: cpu[i]-1 is because R indexes from 1 instead of 0.
+  for (int i = 0; i < LENGTH(cpus); i++)
+    CPU_SET(INTEGER(cpus)[i]-1, &mask);;
+  bail_if(sched_setaffinity(0, sizeof mask, &mask) < 0, "sched_setaffinity()");
+  return R_getaffinity();
+#else 
+  Rf_error("affinity not supported on this system");
+#endif //HAS_AFFINITY  
+}
+
+SEXP R_ncores() {
+  return ScalarInteger(sysconf( _SC_NPROCESSORS_ONLN ));
+}
