1fcb751ceb793943e17d8fbd8ffd6b9d72eeba7c
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-22 13:16:20 +0100

    Start with porting eval_fork stuff

--- src/chroot.c
+++ src/chroot.c
@@ -6,16 +6,7 @@
 #include <errno.h>
 #include <string.h>
 
-/* check for system errors */
-void bail_if(int err, const char * what){
-  if(err)
-    Rf_errorcall(R_NilValue, "System failure for: %s (%s)", what, strerror(errno));
-}
-
-void warn_if(int err, const char * what){
-  if(err)
-    Rf_warningcall(R_NilValue, "System failure for: %s (%s)", what, strerror(errno));
-}
+extern void bail_if(int err, const char * what);
 
 SEXP R_chroot(SEXP path){
   bail_if(chroot(CHAR(STRING_ELT(path, 0))), "chroot()");
